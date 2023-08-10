8c4e01c1b038a20785ed4bd4095a59fce075e460
jasminehilton <jasminehilton1@live.com>
2023-08-10 12:55:10 -0700

    styling on photolistitem and basic page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
+
 import './App.scss';
+
+// const photos = (num) => {
+//   num *
+// };
+
+
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem sample = {sampleDataForPhotoListItem}/>
+      <PhotoListItem sample = {sampleDataForPhotoListItem}/>
+      <PhotoListItem sample = {sampleDataForPhotoListItem}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className=" photo-list photo-list__item photo-list:after " >
+      <img alt="main pic" className="photo-list__image" src={props.sample.imageSource}></img>        <br/>
+      <img alt="profile pic" className="photo-list__user-profile" src={props.sample.profile}></img>
+      <div className="photo-list__user-info photo-list__user-details" >
+        {props.sample.username}
+      </div>
+      <div className="photo-list__user-location" >
+        {props.sample.location.city}, {props.sample.location.country}
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
