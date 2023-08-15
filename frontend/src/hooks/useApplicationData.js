67c233d1ba6df5cbbe30b4cdffd43d9aa2e4aa51
jasminehilton <jasminehilton1@live.com>
2023-08-14 22:25:41 -0700

    custom hook added

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,50 @@
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  SELECT_TOPIC: 'SELECT_TOPIC'
+};
+
+export const photoLabReducer = (state, action) => {
+  switch (action.type) {
+  case ACTIONS.FAV_PHOTO_ADDED:
+    return {
+      ...state,
+      favoritePhotos: [...state.favoritePhotos, action.value]
+    };
+  case ACTIONS.FAV_PHOTO_REMOVED:
+    return {
+      ...state,
+      favoritePhotos: state.favoritePhotos.filter((photoId) => photoId !== action.value)
+    };
+  case ACTIONS.SET_PHOTO_DATA:
+    return {
+      ...state,
+      photos: action.value
+    };
+  case ACTIONS.SET_TOPIC_DATA:
+    return {
+      ...state,
+      topics: action.value
+    };
+  case ACTIONS.SELECT_PHOTO:
+    return {
+      ...state,
+      selectedPhoto: action.value
+    };
+  case ACTIONS.DISPLAY_PHOTO_DETAILS:
+    return {
+      ...state,
+      displayModal: action.value
+    };
+  case ACTIONS.SELECT_TOPIC:
+    return {
+      ...state,
+      selectedTopic: action.value
+    };
+  }
+  return state;
+};
